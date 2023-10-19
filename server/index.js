b57e42214c942b064598a8e6637520f3bf5cb5a1
hushlovies <canariakaira@gmail.com>
2023-10-19 14:13:50 +0200

    n

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ const express =require("express");
 const app=express();
 const PORT= 8081;
 const db= require("./db");
-
+routes=require('./Controllers/tpController.controller')
 app.listen(PORT,
     ()=>console.log(`running on http://localhost:${PORT}`)
 );
 
-app.get("/", (req,res)=>{
-    db.query(`select * from Utilisateurs`,
-    function(err,res){
-    console.log(res)
-}
-);
-});
+app.use('/products')
+
+// app.get("/", (req,res)=>{
+//     db.query(`select * from produits`,
+//     function(err,res){
+//     console.log(res)
+// }
+// ); 
+// });
 
 // app.get("/test", (req,res)=>{
 //     console.log("mayday");
