af81a95faa26e710c7fa933f40fc1fae6cdd25c3
Thuy_Vy <thithuyvitran1405@gmail.com>
2023-10-19 11:17:19 +0200

    Ajoute HTML

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
-import mysql from 'mysql'
-
+// import mysql from 'mysql'
+mysql= require('mysql2')
+router= express.Router()
 const db= mysql.createPool({
 host:'localhost',
 user:'root',
@@ -8,7 +9,8 @@ database:'DataCommande'
 })
 
 //query test
-db.query(`select * from Utilisateurs`,
-(err,res)=>{ return console.log(res)}
-)
+// db.query(`select * from Utilisateurs`,
+// (err,res)=>{ return console.log(res)}
+// )
+module.exports=connect;
 
